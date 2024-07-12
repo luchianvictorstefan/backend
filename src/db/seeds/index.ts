69544b433ccfae33cfd1580c20f50d672f79bb9c
Victor Luchian <luchian_victor@yahoo.com>
2024-07-12 17:24:21 +0300

    pnpm new repo

--- src/db/seeds/index.ts
+++
@@ -1,8 +0,0 @@
-
-export { default as state } from "./state";
-export { default as category } from "./category";
-export { default as statusCatalog } from './statusCatalog';
-export { default as city } from "./city";
-export { default as restaurant } from "./restaurant";
-export { default as user } from './user';
-export { default as order } from './order';
--- src/db/seeds/state.ts
+++
@@ -1,7 +0,0 @@
-import type db from "@/db";
-import states from './data/states.json';
-import { state } from "../schema";
-
-export default async function seed(db: db) {
-  await db.insert(state).values(states);
-}
