2224ed26c1c08e7c70c6485ff0144c09af1f2131
Victor Luchian <luchian_victor@yahoo.com>
2024-07-12 13:46:20 +0300

    env var postgres

--- src/routes/test-route.ts
+++ src/routes/test-route.ts
@@ -1,7 +1,18 @@
 import { Hono } from 'hono';
+import postgres from "postgres";
 
+const config = {
+  user: Bun.env.USERNAME,
+  host: Bun.env.HOST,
+  port: Number(Bun.env.PORT),
+  pass: Bun.env.PASS,
+  database: Bun.env.DATABASE,
+}
+const sql = postgres(config);
+console.log(config)
+const [{ version }] = await sql`SELECT version()`;
 export const testRoute = new Hono()
-.get('/', c => c.json({payload: []}))
+.get('/', c => c.json({payload: [version]}))
 .post('/', c => c.json({payload: []}))
 .delete('/', c => c.json({payload: []}))
 .patch('/', c => c.json({payload: []}));
