4e3b1936344a4659e4a69d3e27f344e7106eaa42
luchianvictorstefan <victor.luchian.ext@buildingminds.com>
2024-07-12 19:33:46 +0300

    zod and drizzle setup

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 // src/app.ts
 import express from 'express';
-
+import { rpcHandler } from "typed-rpc/lib/express";
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello, TypeScript with Express!');
 });
-
+// app.post("/api", rpcHandler(Service));
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
